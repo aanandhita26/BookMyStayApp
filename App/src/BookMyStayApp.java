5f8a466e0c13f22b1fc0fa2622698569a92f722a
aanandhita26 <a4aanandhita@gmail.com>
2026-03-14 23:31:56 +0530

    Aanandhita added UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,20 @@
+/**
+ * BookMyStayApp
+ *
+ * Entry point for the BookMyStay Hotel Booking Application.
+ * This program demonstrates how a Java application starts execution
+ * using the main() method and displays a welcome message to the user.
+ *
+ * The application prints the system name and version when launched.
+ *
+ * @author Aanandhita
+
+ */
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
